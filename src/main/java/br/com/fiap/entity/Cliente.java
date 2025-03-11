4f4ecef893ae3489ecf8b1e9db869adb0654c2ef
SunaUezuri <wesley.jane.santos@gmail.com>
2025-03-11 20:56:14 -0300

    Iniciando a modelagem da classe cliente.

--- src/main/java/br/com/fiap/entity/Cliente.java
+++ src/main/java/br/com/fiap/entity/Cliente.java
@@ -1,4 +1,22 @@
 package br.com.fiap.entity;
 
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "TDS_T_CLIENTE")
+@SequenceGenerator(name = "cliente", sequenceName = "SQ_TDS_T_CLIENTE", allocationSize = 1)
 public class Cliente {
+
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cliente")
+    private Integer id;
+
+    @Column(name = "nm_cliente", nullable = false, length = 100)
+    private String nome;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar dataNascimento;
+
 }
